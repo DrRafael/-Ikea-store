42d403f2217f516b0b8967d4ff09bab254e891a5
Rafael <rfl777@mail.ru>
2020-10-27 12:50:31 -0300

    openSubMenu

--- script/main.js
+++ script/main.js
@@ -1,1 +1,36 @@
-alert('Hello world')
+
+
+// day1
+
+const btnBurger = document.querySelector('.btn-burger');
+const catalog = document.querySelector('.catalog');
+const overLay = document.querySelector('.overlay');
+const btnClose = document.querySelector('.btn-close');
+const subCatalog = document.querySelector('.subcatalog');
+const subcatalogHeader = document.querySelector('.subcatalog-header');
+
+const openMenu = () => {
+        catalog.classList.add('open');
+        overLay.classList.add('active');
+};
+
+const closeMenu = () => {
+    catalog.classList.remove('open');
+    overLay.classList.remove('active');
+};
+
+const openSubMenu = (event) => {
+    event.preventDefault();
+    const target = event.target;
+    const itemList = target.closest('.catalog-list__item');
+    if (itemList){
+        subcatalogHeader.innerHTML = target.innerHTML;
+        subCatalog.classList.add('subopen');
+    };
+};
+
+
+btnBurger.addEventListener('click', openMenu);
+btnClose.addEventListener('click', closeMenu);
+overLay.addEventListener('click', closeMenu);
+catalog.addEventListener('click', openSubMenu);
