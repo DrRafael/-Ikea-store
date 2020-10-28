06ca5408d79195e2898caa8d32c5adf9ce385fe5
Rafael <rfl777@mail.ru>
2020-10-28 18:45:20 -0300

    created menu, subMenu, overlay

--- script/main.js
+++ script/main.js
@@ -1,36 +1,42 @@
 
-
-// day1
-
 const btnBurger = document.querySelector('.btn-burger');
 const catalog = document.querySelector('.catalog');
-const overLay = document.querySelector('.overlay');
 const btnClose = document.querySelector('.btn-close');
 const subCatalog = document.querySelector('.subcatalog');
-const subcatalogHeader = document.querySelector('.subcatalog-header');
+const subcatalogHeader = document.querySelector('.subcatalog-header')
+const btnReturn = document.querySelector('.btn-return');
+
+const overlay = document.createElement('div');
+overlay.classList.add('overlay');
+document.body.insertAdjacentElement('beforeend', overlay);
+
 
 const openMenu = () => {
         catalog.classList.add('open');
-        overLay.classList.add('active');
+        overlay.classList.add('active');
 };
 
 const closeMenu = () => {
+    closeSubMenu();
     catalog.classList.remove('open');
-    overLay.classList.remove('active');
+    overlay.classList.remove('active');    
 };
 
 const openSubMenu = (event) => {
     event.preventDefault();
-    const target = event.target;
-    const itemList = target.closest('.catalog-list__item');
+    const itemList = event.target.closest('.catalog-list__item');
     if (itemList){
-        subcatalogHeader.innerHTML = target.innerHTML;
+        subcatalogHeader.innerHTML = itemList.innerHTML;
         subCatalog.classList.add('subopen');
     };
 };
 
+const closeSubMenu = () => {
+    subCatalog.classList.remove('subopen');
+}
 
 btnBurger.addEventListener('click', openMenu);
 btnClose.addEventListener('click', closeMenu);
-overLay.addEventListener('click', closeMenu);
+overlay.addEventListener('click', closeMenu);
 catalog.addEventListener('click', openSubMenu);
+btnReturn.addEventListener('click', closeSubMenu);
